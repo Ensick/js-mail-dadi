214ff05d1eb0763e0e2a9d5a92ecd3bc5ad4da01
Ensick <fede.censi96@gmail.com>
2022-10-24 14:43:02 +0200

    Inizio esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia già presente nella lista di email per controllare se può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// scrivere quante partite ha vinto il giocatore
+// scrivere quante partite ha vinto il computer
+
+let listaemail = ["fede.censi@gmail.com" , "marco96@gmail.com" , "kevin96@gmail.com"]
+
+let scriviemail = document.getElementById("scriviemail")
+
+
